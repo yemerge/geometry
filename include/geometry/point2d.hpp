24d7042936bc62d2571719b85bf16488d909f0f4
yemerge <bohmbohmpow@gmail.com>
2023-12-26 20:35:41 +0900

    Added basic description for point2d.hpp

---
+++ include/geometry/point2d.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file geometry/point2d.hpp
+ * @author yemerge (bohmbohmpow@gmail.com)
+ * @brief Point class declaration with 2-dimension
+ * @version 0.1
+ * @date 2023-12-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+// Copyright (c) 2023 Programmers, All Rights Reserved.
+// Authors : yemerge
+
+#ifndef YEMERGE__GEOMETRY__POINT_2D_HPP_
+#define YEMERGE__GEOMETRY__POINT_2D_HPP_
+
+#endif
